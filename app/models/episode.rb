5c5b6378ff0595bf62bbc1d46e8cb3505eeee7c1
Prem Sichanugrist <s@sikachu.com>
2012-02-14 17:34:53 -0500

    Initial commit

---
+++ app/models/episode.rb
@@ -0,0 +1,3 @@
+class Episode < ActiveRecord::Base
+  has_attached_file :attachment
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Foo::Application.config.secret_token = 'b0703b22da94377c6f467a3ef68cc8fbedb5cea6a8751f07661047cf1ec891e08aa6a1e129cb12beaef5cccf2a42be5814aa92910b03d61ce090ee22c747f9fa'
